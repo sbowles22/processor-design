4be5351668d78b902a8930eb6c61ac2bc53fbaf0
kwaymire22choate <90625694+kwaymire22choate@users.noreply.github.com>
2021-10-14 13:56:54 -0400

    Added nightynight.asm, LED program for midterm
    Added nightynight.asm, LED program for midterm

---
+++ midterm-processor/programs/nightynight.asm
@@ -0,0 +1,15 @@
+begin: 
+	SET R1 253 //reg address button 
+	BEQ R1, 1
+	SET R0, begin
+		
+lightLED:
+	SET R2, 250 //reg address led[0]
+	SET R3, 251 //reg address led[1]
+	SET R4, 252 //reg address led[2] 
+	STORE R2, R1, 0 
+	STORE R3, R1, 0
+	STORE R4, R1, 0
+	SET R0, begin
+	
+	 

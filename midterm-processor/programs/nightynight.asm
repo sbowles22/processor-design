c85ab0a1c50abfa3b223de367b6f4fcb9651c957
kwaymire22choate <90625694+kwaymire22choate@users.noreply.github.com>
2021-10-14 14:38:50 -0400

    Update nightynight.asm

--- midterm-processor/programs/nightynight.asm
+++ midterm-processor/programs/nightynight.asm
@@ -8,9 +8,9 @@ lightLED:
 	SET R2, 250 //reg address led[0]
 	SET R3, 251 //reg address led[1]
 	SET R4, 252 //reg address led[2] 
-	STORE R2, R1, 0 
-	STORE R3, R1, 0
-	STORE R4, R1, 0
+	STORE R1, R2, 0 
+	STORE R1, R3, 0
+	STORE R1, R4, 0
 	SET R0, begin
 	
 	 
